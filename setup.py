b5fb4f6cad8df5622e8f32bdcd03bc9d3d609d4f
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2018-06-08 11:31:30 +0900

    Explicitly specified long description type

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     version=version,
     description=desc,
     long_description=long_desc,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     install_requires=dependencies,
